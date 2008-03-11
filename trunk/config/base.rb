b28422a857c1710b5c24905b7ebe241590754f26
Jacob Fugal <lukfugl@gmail.com>
2008-03-11 20:50:48 +0000

    basic model infrastructure

---
+++ trunk/config/base.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'active_record'
+require 'yaml'
+
+ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+dblog = File.join(ROOT, 'log', 'database.log')
+dbconfig = File.join(ROOT, 'config', 'database.yml')
+
+ActiveRecord::Base.logger = Logger.new(File.open(dblog, 'a'))
+ActiveRecord::Base.establish_connection(YAML::load(File.open(dbconfig)))
