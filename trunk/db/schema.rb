cb33994db9f2a74ef2410abcabee923bf7f74478
Jacob Fugal <lukfugl@gmail.com>
2008-03-11 21:11:11 +0000

    just use db/schema.rb instead of db/migrations (for now)

---
+++ trunk/db/schema.rb
@@ -0,0 +1,7 @@
+ActiveRecord::Schema.define do
+  #create_table "tablename", :force => true do |t|
+  #  t.column :other_id, :integer
+  #  t.column :name,     :string
+  #  t.column :position, :integer
+  #end
+end
