edd86ff0dfca663561209b4a23e9b15232278c2e
Jacob Fugal <lukfugl@gmail.com>
2008-03-14 23:09:44 +0000

    add the complex seat associations (hand, alhambra, reserve board)

---
+++ trunk/app/models/hand.rb
@@ -0,0 +1,6 @@
+require 'models/card'
+require 'models/hand_card'
+
+# a module to extend on the Seat model's hand association
+module Hand
+end
---
+++ trunk/app/models/reserve_board.rb
@@ -0,0 +1,6 @@
+require 'models/tile'
+require 'models/reserve_board_tile'
+
+# a module to extend on the Seat model's reserve_board association
+module ReserveBoard
+end
