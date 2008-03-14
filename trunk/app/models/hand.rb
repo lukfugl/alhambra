9a650da5b802507a03b20abb401964ddd1544db6
Jacob Fugal <lukfugl@gmail.com>
2008-03-14 23:54:02 +0000

    flesh out the seat.hand association, with tests

--- trunk/app/models/hand.rb
+++ trunk/app/models/hand.rb
@@ -3,4 +3,15 @@ require 'models/hand_card'
 
 # a module to extend on the Seat model's hand association
 module Hand
+  def setup
+    clear
+  end
+
+  def cards
+    map{ |link| link.card }
+  end
+
+  def add_cards(*cards)
+    cards.each{ |card| build(:card => card) }
+  end
 end
--- trunk/test/unit/hand_test.rb
+++ trunk/test/unit/hand_test.rb
@@ -3,8 +3,25 @@ require 'models/seat'
 require ROOT + '/test/sqlite_transactional_tests'
 
 class HandTest < Test::Unit::TestCase
-  def test_truth
-    assert true
+  def setup
+    @seat = Seat.new
+  end
+
+  def test_setup
+    @seat.hand.setup
+    assert_equal 0, @seat.hand.size
+  end
+
+  def test_add_cards
+    cards = (1..5).map{ |i| Card.create(:value => i, :currency => "denar") }
+    @seat.hand.add_cards(*cards)
+    assert_equal cards, @seat.hand.map{ |link| link.card }.sort_by{ |card| card.value }
+  end
+
+  def test_cards
+    cards = (1..5).map{ |i| Card.create(:value => i, :currency => "denar") }
+    @seat.hand.add_cards(*cards)
+    assert_equal cards, @seat.hand.cards.sort_by{ |card| card.value }
   end
 
   include SqliteTransactionalTests
