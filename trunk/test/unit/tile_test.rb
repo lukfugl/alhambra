670aea04e0bd8fb73a51d9ace7d8ac7307bfb718
Jacob Fugal <lukfugl@gmail.com>
2008-03-15 17:17:48 +0000

    testing wall accessor

--- trunk/test/unit/tile_test.rb
+++ trunk/test/unit/tile_test.rb
@@ -1,8 +1,22 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class TileTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_reading_walls
+    # each direction
+    tile = Tile.new(:walls => "1000")
+    assert_equal [ "east" ], tile.walls
+
+    tile = Tile.new(:walls => "0100")
+    assert_equal [ "south" ], tile.walls
+
+    tile = Tile.new(:walls => "0010")
+    assert_equal [ "west" ], tile.walls
+
+    tile = Tile.new(:walls => "0001")
+    assert_equal [ "north" ], tile.walls
+
+    # and with multiple
+    tile = Tile.new(:walls => "1011")
+    assert_equal [ "east", "west", "north" ], tile.walls
   end
 end
