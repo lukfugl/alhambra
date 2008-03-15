eb3c0a87b1237de66007734b2293de11c5274605
Jacob Fugal <lukfugl@gmail.com>
2008-03-15 00:01:39 +0000

    flesh out the seat.reserve_board association, with tests

--- trunk/app/models/reserve_board.rb
+++ trunk/app/models/reserve_board.rb
@@ -3,4 +3,15 @@ require 'models/reserve_board_tile'
 
 # a module to extend on the Seat model's reserve_board association
 module ReserveBoard
+  def setup
+    clear
+  end
+
+  def tiles
+    map{ |link| link.tile }
+  end
+
+  def add_tiles(*tiles)
+    tiles.each{ |tile| build(:tile => tile) }
+  end
 end
