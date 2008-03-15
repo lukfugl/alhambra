d7f38806855c76534a2686d0d5c437e211e8432f
Jacob Fugal <lukfugl@gmail.com>
2008-03-15 17:15:26 +0000

    never going to change walls of a tile in normal usage; in the rare cases I need to change/set the walls of the tile I can just use the flag code

--- trunk/app/models/tile.rb
+++ trunk/app/models/tile.rb
@@ -16,22 +16,8 @@ class Tile < ActiveRecord::Base
     'north'
   ]
 
-  # accessor overrides to accept and emit a subset of WALL_DIRECTIONS rather
-  # than the raw flag string (assignment can still accept the raw flag string,
-  # however)
-  def walls=(*walls)
-    if walls.size == 1 && (walls.first =~ /^[01]{4}$/ || Array === walls.first)
-      walls = walls.first
-    end
-
-    if Array === walls
-      walls = WALL_DIRECTIONS.map{ |dir| walls.include?(dir) ? '1' : '0' }
-      walls = walls.join('')
-    end
-
-    super(walls)
-  end
-
+  # accessor override to emit a subset of WALL_DIRECTIONS rather than the raw
+  # flag string
   def walls
     codified_walls = super
     codified_walls.split(//).zip(WALL_DIRECTIONS).
