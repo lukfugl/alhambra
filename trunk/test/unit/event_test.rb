22444c4572ed76f54073f8b2ad83b184d9bef0de
Jacob Fugal <lukfugl@gmail.com>
2008-03-19 01:18:18 +0000

    clarify test

--- trunk/test/unit/event_test.rb
+++ trunk/test/unit/event_test.rb
@@ -6,14 +6,16 @@ class EventTest < ActiveSupport::TestCase
     data = { :foo => 1, :bar => 2 }
     event_class.class_eval{ event_data *data.keys }
     event = event_class.new(data)
+
     assert_equal data, event.event_data
     assert_equal 1, event.foo
     assert_equal 2, event.bar
 
     event.save
-    loaded_event = event_class.find(event.id)
-    assert_equal data, loaded_event.event_data
-    assert_equal 1, loaded_event.foo
-    assert_equal 2, loaded_event.bar
+    event.reload
+
+    assert_equal data, event.event_data
+    assert_equal 1, event.foo
+    assert_equal 2, event.bar
   end
 end
