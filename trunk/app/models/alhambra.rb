70373c65b9e2b76d3d39a306198fca8a97f30479
Jacob Fugal <lukfugl@gmail.com>
2008-03-20 05:38:34 +0000

    use the string, not the symbol, for building type

--- trunk/app/models/alhambra.rb
+++ trunk/app/models/alhambra.rb
@@ -1,6 +1,6 @@
 # a module to extend on the Seat model's alhambra association
 module Alhambra
-  LION_FOUNTAIN = Tile.find_by_building_type(:fountain)
+  LION_FOUNTAIN = Tile.find_by_building_type('fountain')
 
   def setup
     clear
