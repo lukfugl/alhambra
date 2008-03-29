d71a9325d77bb11548fd4a040aabb2382e4506c0
Jacob Fugal <lukfugl@gmail.com>
2008-03-29 18:54:01 +0000

    create game with seats on Event::GameCreated, with tests; oddly, having the *test* file present causes a *different* test file to have failures... need to look into this odd bug

--- trunk/app/models/event/game_created.rb
+++ trunk/app/models/event/game_created.rb
@@ -2,5 +2,12 @@ require 'models/event'
 class Event < ActiveRecord::Base
   # game is created in pre-game state
   class GameCreated < Event
+    def effect_in_game
+      build_game(:state => 'pre-game')
+      6.times do |i|
+        game.seats.build(:seat_order => i)
+      end
+      game.save
+    end
   end
 end
