70f113d78e0bea0af798ae5712592ac036795f37
Jacob Fugal <lukfugl@gmail.com>
2009-02-10 20:28:33 -0700

    forgot to add this in the previous commit

---
+++ app/controllers/lobby_event_controller.rb
@@ -0,0 +1,8 @@
+class LobbyEventController < ApplicationController
+  def POST
+    event = handle_posted_event(Event::GameCreated)
+    head :status => :created, :location => event_uri(event)
+  rescue BadRequest
+    head :status => :bad_request
+  end
+end
